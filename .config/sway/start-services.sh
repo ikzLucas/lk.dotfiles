2685ca32d441a06bb46b823d316c966f1820b9f9
Lucas :) <168363697+ikzLucas@users.noreply.github.com>
2025-10-30 21:17:51 -0600

    epic commit message

--- .config/sway/start-services.sh
+++ .config/sway/start-services.sh
@@ -13,9 +13,4 @@ dbus-update-activation-environment --systemd XDG_CURRENT_DESKTOP XDG_SESSION_TYP
 if command -v systemctl >/dev/null 2>&1; then
    systemctl --user restart xdg-desktop-portal-wlr.service
    systemctl --user restart xdg-desktop-portal-gtk.service
-elif command -v rc-service >/dev/null 2>&1; then
-   /etc/user/init.d/pipewire start
-   /etc/user/init.d/pipewire-pulse start
-   /etc/user/init.d/xdg-desktop-portal start
-   /etc/user/init.d/xdg-desktop-portal-wlr start
 fi
