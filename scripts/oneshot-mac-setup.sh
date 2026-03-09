13880063ccb24eb6937fcc54190021e5bdc5577c
Lucas :D <168363697+ikzLucas@users.noreply.github.com>
2026-03-08 21:19:14 -0600

    ansible stuff for mac setup

---
+++ scripts/oneshot-mac-setup.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash  
+
+# dont run this unless you want all the apps I like to have installed, fast key repeat, and dock on the left
+
+# Install homebrew breh
+
+if ! command -v brew; then
+   /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+else
+   echo 'Homebrew already installed'
+fi
+
+brew analytics off
+
+# Ansible for setup
+if ! command -v ansible; then
+   brew install ansible
+fi
+ansible-galaxy collection install community.general
+ansible-playbook -i ./ansible/inventory ./ansible/darwin.yml
+
+# Install the dotfiles
+../install.sh
